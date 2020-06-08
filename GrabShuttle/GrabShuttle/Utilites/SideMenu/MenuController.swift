dc3e30ee2a94935793cb48ed70e614159ba427f2
navneet195 <navneetbaldha195@gmail.com>
2020-06-08 19:58:37 +0530

    add ride booking app

---
+++ GrabShuttle/GrabShuttle/Utilites/SideMenu/MenuController.swift
@@ -0,0 +1,90 @@
+//
+//  MenuController.swift
+//  GrabDriver
+//
+//  Created by Navnit Baldha on 05/06/20.
+//  Copyright © 2020 Navneet Baldha. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+private let cellIdentifier = "MenuCell"
+
+enum MenuOptions: Int, CaseIterable {
+    case yourTrips
+    case settings
+    case logout
+    
+    var description: String {
+        switch self {
+        case .yourTrips: return "Your Trips"
+        case .settings: return "Settings"
+        case .logout: return "Log Out"
+        }
+    }
+}
+
+protocol MenuControllerDelegate {
+    func didSelect(option: MenuOptions, user: User?)
+}
+
+class MenuController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    var delegate: MenuControllerDelegate?
+    var user: User? {
+        didSet {
+            menuHeader.user = user
+        }
+    }
+    
+    private lazy var menuHeader: MenuHeader = {
+        let frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 140)
+        let view = MenuHeader(frame: frame)
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = Constants.Color.primaryColor
+        configureTableView()
+    }
+        
+    // MARK: - Helper Functions
+    
+    func configureTableView() {
+        tableView.backgroundColor = Constants.Color.primaryColor
+        tableView.separatorStyle = .none
+        tableView.isScrollEnabled = false
+        tableView.rowHeight = 44
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.tableHeaderView = menuHeader
+    }
+}
+
+// MARK: - TableViewDelegate/DataSource
+
+extension MenuController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MenuOptions.allCases.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = MenuOptions(rawValue: indexPath.row)?.description
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let option = MenuOptions(rawValue: indexPath.row) else {return}
+        delegate?.didSelect(option: option, user: user)
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
---
+++ GrabShuttle/GrabShuttle/View/CircularProgressView.swift
@@ -0,0 +1,87 @@
+//
+//  CircularProgressView.swift
+//  GrabShuttle
+//
+//  Created by Navnit Baldha on 06/06/20.
+//  Copyright © 2020 Navneet Baldha. All rights reserved.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+    // MARK: - Properties
+    
+    var progressLayer: CAShapeLayer!
+    var trackLayer: CAShapeLayer!
+    var pulsatingLayer: CAShapeLayer!
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureCircleLayers()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helper Functions
+    
+    private func configureCircleLayers() {
+        pulsatingLayer = circleShapeLayer(strokeColor: .clear, fillColor: .rgb(red: 86, green: 30, blue: 63))
+        layer.addSublayer(pulsatingLayer)
+        
+        trackLayer = circleShapeLayer(strokeColor: .rgb(red: 56, green: 25, blue: 49), fillColor: .clear)
+        layer.addSublayer(trackLayer)
+        
+        progressLayer = circleShapeLayer(strokeColor: .rgb(red: 234, green: 46, blue: 111), fillColor: .clear)
+        layer.addSublayer(progressLayer)
+    }
+    
+    private func circleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: self.frame.width / 2, startAngle: -0.5 * .pi, endAngle: 1.5 * .pi, clockwise: true)
+        
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 12
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = .round
+        layer.position = center
+        
+        return layer
+    }
+    
+    func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue = 1.25
+        animation.duration = 1
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        
+        pulsatingLayer.add(animation, forKey: "pulsing")
+    }
+    
+    func animateProgress(duration: TimeInterval, completion: @escaping() -> Void) {
+        animatePulsatingLayer()
+        
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = duration
+        animation.fromValue = 1
+        animation.toValue = 0
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        
+        progressLayer.strokeEnd = 0
+        progressLayer.add(animation, forKey: "progress")
+        
+        CATransaction.commit()
+    }
+}
